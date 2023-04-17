a5d9d26422d7f8373ffae0dfec88fb324eb66bf3
NATASHA OMARI <natashaonyangoomar@gmail.com>
2023-04-17 17:03:27 +0300

    firstcommit

---
+++ src/views/crops/CropIndex.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <div v-if="floras.length>0">
+            <CropCardComponent :floras="floras" @delete-crop="deleteCrop"></CropCardComponent>
+        </div>
+        <div v-else>
+            <h1 class="text-danger">Crops Loading...</h1>
+        </div>
+        
+    </div>
+</template>
+<script>
+import CropCardComponent from '../../components/farmfresh/CropCardComponent.vue';
+import axios from 'axios'
+export default {
+    components: { CropCardComponent },
+    data(){
+        return {
+            floras:[]
+        };
+    },
+    created () {
+        this.getCrops();
+    },
+    methods: {
+        getCrops(){
+            axios.get('http://127.0.0.1:8000/api/floras').then(res=>{
+                this.floras=res.data;
+                console.log(res.data);
+            })
+        },
+       
+    }
+}
+</script>
+<style>
+    
+</style>
